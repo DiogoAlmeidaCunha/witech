bca7384b70cb5954ba3c9a5cacb3875927578630
Diogo Almeida Cunha <diogocunha588@gmail.com>
2021-03-04 23:51:02 +0000

    features added

--- witech-web/src/app/front-page/front-page.component.ts
+++ witech-web/src/app/front-page/front-page.component.ts
@@ -12,6 +12,43 @@ export class FrontPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  
+  setWork(n){
+    var element1 = document.getElementById("meeting");
+    var element2 = document.getElementById("development");
+    var element3 = document.getElementById("delivery");
+
+    var btn1 = document.getElementById("btn1");
+    var btn2 = document.getElementById("btn2");
+    var btn3 = document.getElementById("btn3");
+
+    btn1.style.background = '#181b2e'
+    btn2.style.background = '#181b2e'
+    btn3.style.background = '#181b2e'
+
+    if(n==1){
+      element1.classList.remove("hidden");
+      element2.classList.add("hidden")
+      element3.classList.add("hidden")
+      btn1.style.background = '#c22ecf'
+
+    }
+
+    if(n==2){
+      element2.classList.remove("hidden");
+      element1.classList.add("hidden")
+      element3.classList.add("hidden")
+      btn2.style.background = '#c22ecf'
+
+    }
+
+    if(n==3){
+      element3.classList.remove("hidden");
+      element1.classList.add("hidden")
+      element2.classList.add("hidden")
+      btn3.style.background = '#c22ecf'
+
+    }
+
+  }
 
 }
